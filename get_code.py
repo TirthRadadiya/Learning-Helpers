86ef061249e93b43473ec8e9e0da21b6860057e6
TirthRadadiya <76026104+TirthRadadiya@users.noreply.github.com>
2023-02-08 10:03:15 +0530

    added extraction based on key press
    first, take a snapshot and press the '`' backtick key. You will have extracted text ready to paste.

--- get_code.py
+++ get_code.py
@@ -2,6 +2,7 @@ import pyperclip
 import pytesseract
 from PIL import ImageGrab
 import time
+import keyboard
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
@@ -9,14 +10,19 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tessera
 # pyperclip.copy(s1)
 # s2 = pyperclip.paste()
 
-while True:
+def work():
     try:
         img = ImageGrab.grabclipboard()
         if img:
             text = pytesseract.image_to_string(img)
-            time.sleep(5)
+            time.sleep(2)
             print(text)
             pyperclip.copy(text)
     except Exception as e:
         # print(e)
         pass
+
+print("start")
+while True:
+    if keyboard.is_pressed('`'):
+        work()
